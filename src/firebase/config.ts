2b3a416b99e3e060fb7437da35fe07ca3375ebe5
Candido Lucas <candidolucaslirabezerra7@gmail.com>
2025-12-04 17:54:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2410777445-ded75",
+  "appId": "1:514948429488:web:5586d6514c9e6471e7ad75",
+  "apiKey": "AIzaSyBePrsO2QuTT6e1uc8PTrctr9UmEXspCO4",
+  "authDomain": "studio-2410777445-ded75.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "514948429488"
+};
